c59aaf1e771e52e0f0125e24e8bfc525985e13fb
V1n1c1us <v.f.diehl@gmail.com>
2020-04-08 23:02:47 -0300

    List and Create Projects with API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
+import './App.css';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  },[]);
+
+  async function handleAddProject() {
+    const response = await api.post('/projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Vinícius Diehl de Franceschi',  
+    });
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <Header title="Header Title">
         <ul>
-          <li>Meni 1</li>
-          <li>Menu 2</li>
+          { projects.map(project => (
+            <li key={ project.id }>{ project.title }</li>
+          ))}
         </ul>
-      </Header>
-      <Header title="Header Title">
-        <ul>
-          <li>Meni 1</li>
-          <li>Menu 2</li>
-          <li>Menu 2</li>
-          <li>Menu 2</li>
-          <li>Menu 2</li>
-        </ul>
+        <button onClick={handleAddProject}>Adicionar Projeto</button>
       </Header>
     </>
   );
